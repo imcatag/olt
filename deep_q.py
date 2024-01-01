45bc6911b264a518d415bd1639aa3357d0649010
Andrei Hodoroaga <andreihodoroaga36@gmail.com>
2024-01-01 21:09:28 +0200

    deep q possibly working

--- deep_q.py
+++ deep_q.py
@@ -1,41 +1,16 @@
 import random
 import numpy as np
 from main import *
-from helper import *
-
-
-# we need the next piece from c# in order to generate the next boards..
-class GameImageState:
-    # takes a 21 * 15 game state and pads it to make it square
-    def __init__(
-        self,
-        actual_state: List[List[int]],
-        piece: Piece,
-        held_piece: Piece,
-        next_bag_piece: Piece,
-    ):
-        self.repr = np.pad(
-            actual_state, ((0, 0), (0, 6)), mode="constant", constant_values=0
-        )
-        self.game_state = self.get_game_state(actual_state)
-        self.piece = piece
-        self.held_piece = held_piece
-        self.next_bag_piece = next_bag_piece
-
-    def get_game_state(self, actual_state: List[List[int]]) -> GameState:
-        board = Board(board=np.array(actual_state)[-20:, :10])
-        return GameState(board, self.piece)
-
-    # TODO: implement this
-    def get_actual_state(self, game_state: GameState):
-        return []
-
-    def generate_children(self):
-        return [self.get_actual_state(x) for x in self.game_state.generateChildren()]
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Flatten
 
 
 class DQNAgent:
-    def __init__(self, state_size=21 * 21):
+    def __init__(self, input_shape = (20, 20, 1)):
+        self.empty_board = Board()
+        self.state = GameState(self.empty_board, pieceQueue[0])
         self.lr = 0.001
         self.gamma = 0.95
         self.exploration_prob = 1.0
@@ -48,25 +23,35 @@ class DQNAgent:
         self.max_memory_buffer = 2000
 
         # Define a model that returns the expected value of the given state + action
-        self.model = Sequential(
-            [
-                Dense(units=24, input_dim=state_size, activation="relu"),
-                Dense(units=24, activation="relu"),
-                Dense(units=action_size, activation="linear"),
-            ]
-        )
-        self.model.compile(
-            loss="mse", optimizer=Adam(lr=self.lr)
-        )  # RMSProp could be another choice here
+        self.model = Sequential([
+            Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape),
+            MaxPooling2D(pool_size=(2, 2)),
+            Conv2D(64, kernel_size=(3, 3), activation='relu'),
+            MaxPooling2D(pool_size=(2, 2)),
+            Flatten(),
+            Dense(128, activation='relu'),
+            Dense(1, activation='linear')  # Output layer for regression with linear activation
+        ])
 
-    def get_approx_Q(state: GameImageState) -> float:
-        # add img std? https://www.tensorflow.org/api_docs/python/tf/image/per_image_standardization
-        return self.model.predict(state.repr)
+        self.model.summary()
 
-    def get_next_state(self, next_states: List["GameImageState"]) -> GameImageState:
-        if random.uniform(0, 1) < self.epsilon:
+        # Compile the model
+        self.model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae'])  # RMSProp could be another choice here
+
+    def get_model_input_from_repr(self, state_repr: List[List[int]]):
+        return np.array(state_repr).reshape(-1, 20, 20, 1)
+
+    # game_state_repr = the 20x20 board
+    def get_approx_Q(self, game_state_repr: List[List[int]], play_mode=False) -> float:
+        # add img std? https://www.tensorflow.org/api_docs/python/tf/image/per_image_standardization]
+        input_data = self.get_model_input_from_repr(game_state_repr)
+        verbose = 0 if play_mode else 1
+        return self.model.predict(input_data, verbose=verbose)
+
+    def get_next_state(self, next_states: List["GameState"]) -> GameState:
+        if random.uniform(0, 1) < 1 + self.exploration_prob: # TODO: remove 1
             return random.choice(next_states)
-        return max(next_states, key=lambda x: self.get_approx_Q(x))
+        return max(next_states, key=lambda x: self.get_approx_Q(x.get_game_repr()))
 
     def update_exploration_probability(self):
         self.exploration_prob = self.exploration_prob * np.exp(
@@ -77,9 +62,9 @@ class DQNAgent:
         # current_state is actually the current state-action pair
         self.memory_buffer.append(
             {
-                "current_state": current_state,
+                "current_state_board": current_state.get_game_repr(), # "board" is actually game state repr
                 "reward": reward,
-                "next_state": next_state,
+                "next_state_board": next_state.get_game_repr(),
                 "terminated": terminated,
             }
         )
@@ -93,36 +78,62 @@ class DQNAgent:
         training_labels = []
 
         for experience in batch:
-            current_q = self.get_approx_Q(experience["current_state"])
+            current_q = self.get_approx_Q(experience["current_state_board"])
             target_q = experience["reward"]
             if not experience["terminated"]:
-                target_q += self.gamma * self.model.predict(experience["next_state"])
+                input_data = self.get_model_input_from_repr(experience["next_state_board"])
+                target_q += self.gamma * self.model.predict(input_data)
 
-            training_data.append(experience["current_state"])
+            training_data.append(experience["current_state_board"])
             training_labels.append(target_q - current_q)
 
         self.model.fit(np.array(training_data), np.array(training_labels))
 
     def train(self, n_episodes=10):
         for _ in range(n_episodes):
-            current_state = [] # get the current state 
+            print('<------------------------------->')
+            print('done episode')
+            print('exploration rate: ', self.exploration_prob)
+            print('<------------------------------->')
+            self.state = GameState(self.empty_board, pieceQueue[self.state.pieceCount + 1])
 
             while True:
-                next_possible_states = current_state.generate_children()
-                next_state = self.get_next_state(next_possible_states)
-                reward = 0 # compute reward here
-
+                next_possible_states = self.state.generateChildren()
                 terminated = len(next_possible_states) == 0
-
-                self.store_episode(current_state, reward, next_state, terminated) 
 
                 if terminated:
                     self.update_exploration_probability()
                     break
                 
-                current_state = next_state
+                next_state = self.get_next_state(next_possible_states)
+                reward = next_state.evaluation # not the best reward
+
+                self.total_steps += 1
+                self.store_episode(self.state, reward, next_state, terminated) 
+                
+                self.state = deepcopy(next_state)
 
             if self.total_steps >= self.batch_size:
                 self.train_episode()
 
         self.model.save_weights('recent_weights.hdf5')
+
+    def play(self):
+        self.model.load_weights('recent_weights.hdf5')
+        pieceQueue = []
+        for _ in range(10000):
+            shuffle(defaultbag)
+            pieceQueue += defaultbag
+
+        board = Board()
+        state = GameState(board, pieceQueue[0])
+
+        while True:
+            next_possible_states = state.generateChildren()
+
+            if len(next_possible_states) == 0:
+                break
+
+            next_state = max(next_possible_states, key=lambda x: self.get_approx_Q(x.get_game_repr(), True))
+            print(state)
+            state = deepcopy(next_state)
